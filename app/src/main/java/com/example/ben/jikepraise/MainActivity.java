139797582f2daaa624b337f6274fa965ef241b94
bennnnnnnnn <zhoubangquan1992@gmail.com>
2017-10-18 15:23:55 +0800

    添加效果图

--- app/src/main/java/com/example/ben/jikepraise/MainActivity.java
+++ app/src/main/java/com/example/ben/jikepraise/MainActivity.java
@@ -3,11 +3,7 @@ package com.example.ben.jikepraise;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import com.example.ben.jikepraise.View.JiKePraiseView;
-
 public class MainActivity extends AppCompatActivity {
-
-    private JiKePraiseView mJiKePraiseView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
